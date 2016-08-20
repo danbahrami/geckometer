5048774c647a0d4679e5227a905a3c7a2461489d
danbahrami <dan.bahrami@googlemail.com>
2016-08-20 12:51:57 +0100

    Stubbed out a util to get percentage of a value between a min and max value
    Added tests for getPercentage

---
+++ src/utils.js
@@ -0,0 +1,19 @@
+/**
+ * getPercentage returns a percentage representing where a value
+ * sits in the range defined by a min and max value. If the
+ * minimum where always zero this would simply be a case
+ * of calculating the percentage that the value is of
+ * the max (i.e (value / max) * 100) but this func
+ * takes into account that min is not fixed.
+ *
+ * Example getPercentage(100, 200, 57) = 57
+ *
+ * @param {number} min - The lowest possible number in the range
+ * @param {number} max - The highest possible number in the range
+ * @param {number} value - The value
+ *
+ * @returns {number} The percentage as an integer between including 0 and 100
+ */
+export const getPercentage = (min, max, value) => {
+    
+};
---
+++ test/utils/getPercentage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { expect } from "chai";
+import { getPercentage } from "../../src/utils";
+
+describe("getPercentage util", () => {
+    /*
+     * It should return the correct percentage if min is zero
+     */
+    it("returns the correct percentage if min is zero", () => {
+        const percent = getPercentage(0, 100, 56);
+
+        expect(percent).to.equal(56);
+    });
+
+    /*
+     * It should return the correct percentage if min is not zero
+     */
+    it("returns the correct percentage if min is not zero", () => {
+        const percent = getPercentage(100, 200, 156);
+
+        expect(percent).to.equal(56);
+    });
+
+    /*
+     * It should round to the nearest integer
+     */
+    it("rounds to the nearest integer", () => {
+        const percent = getPercentage(1, 3, 1);
+
+        expect(percent).to.equal(33);
+    });
+
+});
