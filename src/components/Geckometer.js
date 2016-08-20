83689d6f684bad56c9d0a241e2feee904f044a22
danbahrami <dan.bahrami@googlemail.com>
2016-08-20 13:03:51 +0100

    Geckometer component now displays value, percentage and target value with a nice doghnut graph Added styling to Geckometer

--- src/components/Geckometer.js
+++ src/components/Geckometer.js
@@ -1,10 +1,28 @@
 import React, { PropTypes as T } from "react";
+import { getPercentage } from "../utils";
+import Doughnut from "./Doughnut";
 import "./Geckometer.css";
 
 const Geckometer = ({ max, min, value }) => {
+    const percent = getPercentage(min, max, value);
+
     return (
         <div className="Geckometer">
-
+            <Doughnut
+                className="Geckometer__doughnut"
+                percent={percent}
+            />
+            <div className="Geckometer__center">
+                <span className="Geckometer__value">
+                    {value.toString()}
+                </span>
+                <span className="Geckometer__percentage">
+                    {percent.toString()}%
+                </span>
+            </div>
+            <div className="Geckometer__target">
+                Your target is {max.toString()}
+            </div>
         </div>
     );
 };
