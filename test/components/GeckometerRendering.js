f61495a5e646ad324546ba72f6abede602311fd9
danbahrami <dan.bahrami@googlemail.com>
2016-08-20 13:27:09 +0100

    Added test to check that Geckometer displays the currency symbol when there is one

--- test/components/GeckometerRendering.js
+++ test/components/GeckometerRendering.js
@@ -36,4 +36,35 @@ describe("Geckometer component", () => {
 
         expect(wrapper.find(".Geckometer__target").text()).to.contain("100");
     });
+
+    /*
+     * It should display the correct currency symbol when a currency code is provided
+     */
+    it("displays the correct currency symbol when a currency code is provided", () => {
+        const poundWrapper = shallow(
+            <Geckometer min={0} max={100} value={50} currencyCode="GBP" />
+        );
+
+        const euroWrapper = shallow(
+            <Geckometer min={0} max={100} value={50} currencyCode="EUR" />
+        );
+
+        const dollarWrapper = shallow(
+            <Geckometer min={0} max={100} value={50} currencyCode="USD" />
+        );
+
+        const swissFrancWrapper = shallow(
+            <Geckometer min={0} max={100} value={50} currencyCode="CHF" />
+        );
+
+        expect(poundWrapper.find(".Geckometer__value").text()).to.equal("£50");
+        expect(euroWrapper.find(".Geckometer__value").text()).to.equal("€50");
+        expect(dollarWrapper.find(".Geckometer__value").text()).to.equal("$50");
+        expect(swissFrancWrapper.find(".Geckometer__value").text()).to.equal("CHF50");
+
+        expect(poundWrapper.find(".Geckometer__target").text()).to.contain("£");
+        expect(euroWrapper.find(".Geckometer__target").text()).to.contain("€");
+        expect(dollarWrapper.find(".Geckometer__target").text()).to.contain("$");
+        expect(swissFrancWrapper.find(".Geckometer__target").text()).to.contain("CHF");
+    })
 });
