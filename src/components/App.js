eebaee0a376932139bdbd67031931a5691a63c69
danbahrami <dan.bahrami@googlemail.com>
2016-08-20 12:20:22 +0100

    Shifted redux setup so that the provider is added in index.js instead of App.js Now App is a connected component Added Geckometer component to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,19 @@
 import React, { Component } from "react";
-import { createStore, applyMiddleware, compose } from "redux";
-import { Provider } from "react-redux";
-import thunkMiddleware from "redux-thunk";
-import reducer from "../redux/reducer";
-import state from "../redux/state";
-
-const store = createStore(
-    reducer,
-    state,
-    compose(
-        applyMiddleware(thunkMiddleware),
-        window.devToolsExtension ? window.devToolsExtension() : f => f
-    )
-);
+import { connect } from "react-redux";
+import Geckometer from "./Geckometer";
 
 class App extends Component {
     render() {
         return (
-            <Provider store={store}>
-                <div className="App">
-                    Hey
-                </div>
-            </Provider>
+            <div className="App">
+                <Geckometer
+                    min={0}
+                    max={100}
+                    value={57}
+                />
+            </div>
         );
     }
 }
 
-export default App;
+export default connect(state => state)(App);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import reducer from "./redux/reducer";
+import state from "./redux/state";
+
+const store = createStore(
+    reducer,
+    state,
+    compose(
+        applyMiddleware(thunkMiddleware),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+);
+
 import App from './components/App';
 import "normalize.css";
 import './index.css';
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
