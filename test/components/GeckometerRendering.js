1542c16edf078e2efbc4516f9e05831ad252f127
danbahrami <dan.bahrami@googlemail.com>
2016-08-20 11:49:24 +0100

    Added tests for Geckometer component rendering

---
+++ test/components/GeckometerRendering.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { expect } from "chai";
+import Geckometer from "../../src/components/Geckometer";
+
+describe("Geckometer component", function() {
+    /*
+     * It should display the correct value
+     */
+    it("displays the correct value", function() {
+        const wrapper = shallow(
+            <Geckometer min={0} max={100} value={56} />
+        );
+
+        expect(wrapper.find(".Geckometer__value").text()).to.equal("56");
+    });
+
+    /*
+     * It should display the correct percentage
+     */
+    it("displays the correct percentage", function() {
+        const wrapper = shallow(
+            <Geckometer min={100} max={200} value={156} />
+        );
+
+        expect(wrapper.find(".Geckometer__percentage").text()).to.equal("56");
+    });
+    
+    /*
+     * It should display the correct target value
+     */
+    it("displays the correct target value", function() {
+        const wrapper = shallow(
+            <Geckometer min={0} max={100} value={50} />
+        );
+
+        expect(wrapper.find(".Geckometer__target").text()).to.contain("100");
+    });
+});
