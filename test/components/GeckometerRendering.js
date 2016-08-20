df3523b1bef14f325f768577de00cc40c25413c6
danbahrami <dan.bahrami@googlemail.com>
2016-08-20 12:46:05 +0100

    Switched to using arrow functions in tests for consistency

--- test/components/GeckometerRendering.js
+++ test/components/GeckometerRendering.js
@@ -3,11 +3,11 @@ import { shallow } from "enzyme";
 import { expect } from "chai";
 import Geckometer from "../../src/components/Geckometer";
 
-describe("Geckometer component", function() {
+describe("Geckometer component", () => {
     /*
      * It should display the correct value
      */
-    it("displays the correct value", function() {
+    it("displays the correct value", () => {
         const wrapper = shallow(
             <Geckometer min={0} max={100} value={56} />
         );
@@ -18,7 +18,7 @@ describe("Geckometer component", function() {
     /*
      * It should display the correct percentage
      */
-    it("displays the correct percentage", function() {
+    it("displays the correct percentage", () => {
         const wrapper = shallow(
             <Geckometer min={100} max={200} value={156} />
         );
@@ -29,7 +29,7 @@ describe("Geckometer component", function() {
     /*
      * It should display the correct target value
      */
-    it("displays the correct target value", function() {
+    it("displays the correct target value", () => {
         const wrapper = shallow(
             <Geckometer min={0} max={100} value={50} />
         );
