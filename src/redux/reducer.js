75f8af41bf56bff3476dda38a530bad361f9ab18
danbahrami <dan.bahrami@googlemail.com>
2016-08-21 18:19:28 +0100

    Using arrow function expressions for all reducer functions

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,26 +2,20 @@ import createReducer from "./reducerCreator";
 import state from "./state";
 
 export default createReducer({
-    "GET_DATA_START"   : (state) => {
-        return {
-            ...state,
-            data    : null,
-            error   : null,
-            loading : true
-        };
-    },
-    "GET_DATA_SUCCESS" : (state, data) => {
-        return {
-            ...state,
-            data,
-            loading : false
-        };
-    },
-    "GET_DATA_FAILURE" : (state, error) => {
-        return {
-            ...state,
-            error,
-            loading : false
-        };
-    }
+    "GET_DATA_START"   : (state) => ({
+        ...state,
+        data    : null,
+        error   : null,
+        loading : true
+    }),
+    "GET_DATA_SUCCESS" : (state, data) => ({
+        ...state,
+        data,
+        loading : false
+    }),
+    "GET_DATA_FAILURE" : (state, error) => ({
+        ...state,
+        error,
+        loading : false
+    })
 }, state);
