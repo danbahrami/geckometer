8787dd0a116c84298bb1eba1c111ca66640bf5ff
danbahrami <dan.bahrami@googlemail.com>
2016-08-21 06:32:01 +0100

    Added state handling to reducer function

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -2,19 +2,26 @@ import createReducer from "./reducerCreator";
 import state from "./state";
 
 export default createReducer({
-    "GET_DATA_START" : (state) => {
+    "GET_DATA_START"   : (state) => {
         return {
-            ...state
+            ...state,
+            data    : null,
+            error   : null,
+            loading : true
         };
     },
     "GET_DATA_SUCCESS" : (state, data) => {
         return {
-            ...state
+            ...state,
+            data,
+            loading : false
         };
     },
     "GET_DATA_FAILURE" : (state, error) => {
         return {
-            ...state
+            ...state,
+            error,
+            loading : false
         };
     }
 }, state);
