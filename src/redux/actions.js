2785c0f7fce1a4d1375cd1a83dfa1dbcf61de4fe
danbahrami <dan.bahrami@googlemail.com>
2016-08-21 06:57:07 +0100

    getData action is now making a network request to the data endpoint and dispatching according actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -6,6 +6,18 @@ export const getDataFailure = createAction("GET_DATA_FAILURE");
 
 export const getData = () => {
     return (dispatch) => {
+        dispatch(getDataStart());
+
         //Make a network request to our data source
+        fetch("https://widgister.herokuapp.com/challenge/frontend")
+            .then((response) => {
+                return response.json();
+            })
+            .then((json) => {
+                dispatch(getDataSuccess(json));
+            })
+            .catch((error) => {
+                dispatch(getDataFailure(error));
+            });
     };
 };
