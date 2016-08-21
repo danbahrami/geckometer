7f63f5a917dd16c42537e544f71cbaf9bb0f8a61
danbahrami <dan.bahrami@googlemail.com>
2016-08-21 07:20:23 +0100

    Laid out an example app do display Geckometer with a refresh button to fetch new data

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,45 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Geckometer from "./Geckometer";
+import { getData } from "../redux/actions";
+import "./App.css";
 
 class App extends Component {
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData() {
+        const { dispatch } = this.props;
+
+        dispatch(getData());
+    }
+
     render() {
+        const { data, error, loading } = this.props;
+
         return (
             <div className="App">
-                <Geckometer
-                    min={0}
-                    max={100}
-                    value={57}
-                />
+                {error ? (
+                    <span className="App__errorMessage">
+                        "Uh oh! something went wrong... but to be fair, this is just a coding test"
+                    </span>
+                ) : (
+                    <Geckometer
+                        min={data ? data.min : undefined}
+                        max={data ? data.max : undefined}
+                        value={data ? data.value : undefined}
+                        currencyCode={data ? data.unit : undefined}
+                        loading={loading}
+                    />
+                )}
+
+                <button 
+                    className="App__refreshButton" 
+                    onClick={!loading ? this.getData.bind(this) : undefined}
+                >
+                    {loading ? "Fetching data..." : "Refresh data"}
+                </button>
             </div>
         );
     }
